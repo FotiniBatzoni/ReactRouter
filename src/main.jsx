5b91d58c67ae3f289fbcecf72316c5d4d02b3d3f
fotini <f.batzoni@gmail.com>
2022-10-15 14:40:05 +0300

    Mutatons Without Navigation

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import {
 } from 'react-router-dom'
 import Root, {loader as rootLoader, action as rootAction} from './routes/root'
 import ErrorPage from './error-page'
-import Contact, {loader as contactLoader,} from './routes/contact'
+import Contact, {loader as contactLoader, action as contactAction,} from './routes/contact'
 import EditContact,{action as editAction} from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
 import Index from "./routes/index";
@@ -30,7 +30,8 @@ const router = createBrowserRouter([
       {
       path:'contacts/:contactid',
       element: <Contact />,
-      loader: contactLoader
+      loader: contactLoader,
+      action: contactAction
     },
     {
       path: "contacts/:contactId/edit",
