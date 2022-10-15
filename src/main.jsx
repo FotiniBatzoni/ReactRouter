2f2a012a41b973d431076c5d46630717351aa68e
fotini <f.batzoni@gmail.com>
2022-10-15 14:06:04 +0300

    Updating data

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,8 @@ import {
 } from 'react-router-dom'
 import Root, {loader as rootLoader, action as rootAction} from './routes/root'
 import ErrorPage from './error-page'
-import Contact from './routes/contact'
+import Contact, {loader as contactLoader,} from './routes/contact'
+import EditContact from "./routes/edit";
 
 import './index.css'
 
@@ -20,7 +21,13 @@ const router = createBrowserRouter([
     action: rootAction,
     children:[{
       path:'contacts/:contactid',
-      element: <Contact /> 
+      element: <Contact />,
+      loader: contactLoader
+    },
+    {
+      path: "contacts/:contactId/edit",
+      element: <EditContact />,
+      loader: contactLoader,
     }]
   }
 ])
