3cfffda8c9b1d140bcd5ab39c17168f248ebd54f
fotini <f.batzoni@gmail.com>
2022-10-18 19:27:03 +0300

    Sign In And Sign Up

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,18 +1,14 @@
-import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils'
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+
 
 const Authentication = () =>{
-    const logGoogleUser = async() =>{
-        const response = await signInWithGooglePopup();
-        console.log(response)
-    }
-    return (
-        <div>
-            <h1>Sign In</h1>
-            <button onClick={logGoogleUser}>
-                Sign In with Google Popup
-            </button>
-        </div>
-    )
+   return (
+       <div className="authentication-container">
+           <SignInForm />
+           <SignUpForm />
+       </div>
+   )
 }
 
 export default Authentication
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,29 +1,83 @@
-import { initializeApp } from 'firebase/app'
-import { 
-    getAuth,
-    signInWithRedirect,
-    signInWithPopup,
-    GoogleAuthProvider
-} from 'firebase/auth'
+import { initializeApp } from 'firebase/app';
+import { getAuth, 
+         signInWithRedirect, 
+         signInWithPopup,
+         GoogleAuthProvider,
+         createUserWithEmailAndPassword,
+         signInWithEmailAndPassword
+         } from 'firebase/auth'
+
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDKFnsUxRFa2zXGw7cwP1jg9HXibe63tqI",
-    authDomain: "reactrouter-3326c.firebaseapp.com",
-    projectId: "reactrouter-3326c",
-    storageBucket: "reactrouter-3326c.appspot.com",
-    messagingSenderId: "532325164360",
-    appId: "1:532325164360:web:96aa0463571db3a5feff7d"
+    apiKey: "AIzaSyBOsBcPR4B5qd64yIURfeJKJ35CicwkzII",
+    authDomain: "myeshop-db.firebaseapp.com",
+    projectId: "myeshop-db",
+    storageBucket: "myeshop-db.appspot.com",
+    messagingSenderId: "432783883468",
+    appId: "1:432783883468:web:be21d402905d2b5d761669"
   };
   
   // Initialize Firebase
   const firebaseApp = initializeApp(firebaseConfig);
 
-  const googleProvider = new GoogleAuthProvider()
+  const googleProvider = new GoogleAuthProvider();
+
   googleProvider.setCustomParameters({
-    prompt:'select_account'
+    prompt: "select_account"
   });
 
   export const auth = getAuth();
+  export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+  export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
-  export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
+  export const db= getFirestore();
+
+  export const createUserDocumentFromAuth = async (userAuth, additionalInformation ={}) => {
+    if(!userAuth){
+        return;
+    }
+
+    const  userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if(!userSnapshot.exists()){
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+                ...additionalInformation
+            })
+        }catch(error){
+            console.log('error creating the user', error.message)
+        }
+    }
+
+    return userDocRef;
+  }
+
+  export const createAuthUserWithEmailAndPassword = async(email,password)=>{
+    if(!email || !password){
+        return;
+    }
+   return await createUserWithEmailAndPassword(auth, email, password)
+  }
+  
+  export const signInAuthUserWithEmailAndPassword = async(email,password)=>{
+    if(!email || !password){
+        return;
+    }
+   return await signInWithEmailAndPassword(auth, email, password)
+  }
